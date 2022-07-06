b1368939303760af17501fd7aaf7ef4019dcbabe
Sahil-Prince <106463378+malangyar@users.noreply.github.com>
2022-07-06 09:35:31 +0500

    Update Malang_di_jan.py

--- Malang_di_jan.py
+++ Malang_di_jan.py
@@ -1,14 +1,6 @@
-import os, platform, time
-try:
-    import requests
-except:
-    os.system('pip install requests')
+import os
 os.system('git pull')
-import requests
-bit = platform.architecture()[0]
-if bit == '64bit':
-    print("\n\x1b[1;92m Congratulations ! Your Device Support this Tools")
-    os.system('xdg-open https://youtube.com/channel/UCNAeNWfXgJLaoYTIXwzxWRQ');time.sleep(2)
-    import i
-elif bit == '32bit':
-    print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
+print("\n\x1b[1;92m Congratulations ! Your Device Support this Tools")
+os.system('xdg-open https://youtube.com/channel/UCNAeNWfXgJLaoYTIXwzxWRQ');time.sleep(2)
+import i 
+Main()
