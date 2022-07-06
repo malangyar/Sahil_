08918d40fe57d5a4f2ea89f68581c4b5415aab6c
Sahil-Prince <106463378+malangyar@users.noreply.github.com>
2022-07-06 09:29:26 +0500

    Update Malang_di_jan.py

--- Malang_di_jan.py
+++ Malang_di_jan.py
@@ -9,7 +9,7 @@ bit = platform.architecture()[0]
 if bit == '64bit':
     print("\n\x1b[1;92m Congratulations ! Your Device Support this Tools")
     os.system('xdg-open https://youtube.com/channel/UCNAeNWfXgJLaoYTIXwzxWRQ');time.sleep(2)
-    from Sahil_ import i
+    import 
     Menu()
 elif bit == '32bit':
     print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
