1df7abbae562709e0b078ae4fbb1c08fb495fb5c
Sahil-Prince <106463378+malangyar@users.noreply.github.com>
2022-07-06 09:39:45 +0500

    Update Malang_di_jan.py

--- Malang_di_jan.py
+++ Malang_di_jan.py
@@ -1,5 +1,5 @@
 import os
 os.system('git pull')
 print("\n\x1b[1;92m Congratulations ! Your Device Support this Tools")
-os.system('xdg-open https://youtube.com/channel/UCNAeNWfXgJLaoYTIXwzxWRQ')
+print("\n\x1b[1;96m Tool Is Coded By Mr HACKER")
 import i 
